670710a74af7b6c58bd5a733a02804af63b50535
Enzo-Ro-Haus <enzo.ro.haus@gmail.com>
2024-05-09 09:32:34 -0300

    Primeros archivos

---
+++ MyAppEnzoRosso/src/test/java/com/example/MyAppEnzoRH/MyAppEnzoRhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MyAppEnzoRH;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAppEnzoRhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
